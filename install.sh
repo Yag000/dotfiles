dcbca37a6106bfa55c5a1dc0962d39e65cf95ee5
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-05-25 17:49:20 +0200

    Initial installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+POS=$HOME/dotfiles
+
+ln -s $POS/.aliases $HOME/.aliases
+ln -s $POS/.functions $HOME/.functions
+ln -s $POS/.bashrc $HOME/.bashrc
+ln -s $POS/.tmux.conf $HOME/.tmux.conf
+ln -s $POS/.vimrc $HOME/.vimrc
+ln -s $POS/.zshenv $HOME/.zshenv
+ln -s $POS/.zsh_profile $HOME/.zsh_profile
+ln -s $POS/.zshrc $HOME/.zshrc
