c9eae0ca6f98fa1c04aa85470c8584f2835cc7ce
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-06-08 16:01:03 +0200

    ename bar to main

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -10,6 +10,6 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar "-config=$config_file" example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar "-config=$config_file" main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
