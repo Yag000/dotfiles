714537fac0178d7df8a349232b2e1a1a4cf6366c
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-06-27 18:15:11 +0200

    Customize notificatins with dunst

--- install.sh
+++ install.sh
@@ -11,11 +11,13 @@ mv "$HOME/.zshenv" "$HOME/.old_dotfiles"
 mv "$HOME/.zsh_profile" "$HOME/.old_dotfiles"
 mv "$HOME/.zshrc" "$HOME/.old_dotfiles"
 mv "$HOME/alacritty.yml" "$HOME/.old_dotfiles"
+mv "$HOME/.config/betterlockscreenrc" "$HOME/.old_dotfiles"
 
 # Config folders
 mv "$HOME/.config/nvim" "$HOME/.old_dotfiles"
 mv "$HOME/.config/i3" "$HOME/.old_dotfiles"
 mv "$HOME/.config/picom" "$HOME/.old_dotfiles"
+mv "$HOME/.config/dunst" "$HOME/.old_dotfiles"
 
 # Create copies of the old configuration files before installation
 ln -s "$POS/.aliases" "$HOME/.aliases"
@@ -27,8 +29,10 @@ ln -s "$POS/.zshenv" "$HOME/.zshenv"
 ln -s "$POS/.zsh_profile" "$HOME/.zsh_profile"
 ln -s "$POS/.zshrc" "$HOME/.zshrc"
 ln -s "$POS/alacritty.yml" "$HOME/alacritty.yml"
+ln -s "$POS/betterlockscreen/betterlockscreenrc" "$HOME/.config/betterlockscreenrc"
 
 # Config folders
 ln -s "$POS/nvim" "$HOME/.config/nvim"
 ln -s "$POS/i3" "$HOME/.config/i3"
 ln -s "$POS/picom" "$HOME/.config/picom"
+ln -s "$POS/dunst" "$HOME/.config/dunst"
