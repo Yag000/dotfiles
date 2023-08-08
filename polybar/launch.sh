681abca706367b76c49841b5a0febf0293ff0700
Yago Iglesias <yagoiglesias7@gmail.com>
2023-08-08 12:26:24 +0200

    Add network status to polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,13 +13,13 @@ i=0
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     echo "---" | tee -a /tmp/polybar$i.log
     MONITOR=$m polybar --config="$config_file" --reload main 2>&1 | tee -a /tmp/polybar$i.log & disown
-#    MONITOR=$m polybar --config="$config_file" --reload left 2>&1 | tee -a /tmp/polybarLeft$i.log & disown
- #   sleep 0.1 # To get correct order
-  #  MONITOR=$m polybar --config="$config_file" --reload middle 2>&1 | tee -a /tmp/polybarMiddle$i.log & disown
-   # sleep 0.1
+    #MONITOR=$m polybar --config="$config_file" --reload left 2>&1 | tee -a /tmp/polybarLeft$i.log & disown
+    #sleep 0.1 # To get correct order
+    #MONITOR=$m polybar --config="$config_file" --reload middle 2>&1 | tee -a /tmp/polybarMiddle$i.log & disown
+    #sleep 0.1
     #MONITOR=$m polybar --config="$config_file" --reload rigth 2>&1 | tee -a /tmp/polybarRight$i.log & disown
     i=$((i+1))
 done
 
 
-echo "Bars launched..."
+echo "Bars launched..." 
