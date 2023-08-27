07b3898a11edc4445c7bf49ece44ba31a4de770f
Yago Iglesias <yagoiglesias7@gmail.com>
2023-08-27 20:53:08 +0200

    chore: Some cleaning up of nvim config

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,23 +1,23 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 vim.keymap.set("n", "<leader>gc", function()
-	vim.cmd.Git("commit")
+    vim.cmd.Git("commit")
 end)
 
 vim.keymap.set("n", "<leader>gp", function()
-	vim.cmd.Git("push")
+    vim.cmd.Git("push")
 end)
 
 vim.keymap.set("n", "<leader>gl", function()
-	vim.cmd.Git("pull")
+    vim.cmd.Git("pull")
 end)
 
 vim.keymap.set("n", "<leader>gb", function()
-	vim.cmd.Git("diff")
+    vim.cmd.Git("diff")
 end)
 
 vim.keymap.set("n", "<leader>gd", function()
-	vim.cmd.Gdiffsplit()
+    vim.cmd.Gdiffsplit()
 end)
 
 vim.keymap.set("n", "<leader>go", ":Git push -u origin ", opts)
--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,7 +1,6 @@
-
 vim.keymap.set("n", "<leader>xs", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true}
+    { silent = true, noremap = true }
 )
-vim.keymap.set("n","<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+    { silent = true, noremap = true }
 )
